7f9d354dc7e4be6e210a6a203268fbaffece6278
Consultant - Mohamed Thowfeeq Ahamad <consultant-mohamed.thowfeeq.ahamad@mcb.mu>
2023-10-30 20:52:20 +0530

    sort changea

--- index.js
+++ index.js
@@ -7,20 +7,24 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 app.use(bodyParser.json());
-mongoose.connect(`mongodb+srv://abuka:${process.env.passkey}@cluster0.ltdpswl.mongodb.net/`, {
+const cors = require('cors');
+app.use(cors({
+  origin: '*'
+}));
+mongoose.connect(`mongodb+srv://abuka:221bbakerstreet@cluster0.ltdpswl.mongodb.net/`, {
   useNewUrlParser: true, useUnifiedTopology: true
 })
 
 app.use(express.urlencoded({ extended: false }))
 
 app.get('/urls', async (req, res) => {
-  const shortUrls = await ShortUrl.find({},{'full':1, 'short':1, "createdAt": 1, "_id": 0})
+  const shortUrls = await ShortUrl.find({},{'full':1, 'short':1, "createdAt": 1, "_id": 0}).sort({createdAt: -1})
   res.send(shortUrls)
 })
 
 app.post('/shortUrls', async (req, res) => {
   await ShortUrl.create({ full: req.body.fullUrl })
-  const shortUrls = await ShortUrl.find({},{'full':1, 'short':1,"createdAt": 1, "_id": 0})
+  const shortUrls = await ShortUrl.find({},{'full':1, 'short':1,"createdAt": 1, "_id": 0}).sort({createdAt: -1})
   res.send(shortUrls)
 })
 
